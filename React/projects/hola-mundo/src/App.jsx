928cc8d115b74a979dc0a665a1d36ff4e5257008
Robert Geovanny <robertvasquez2050@gmail.com>
2025-05-25 07:38:02 -0400

    Primera clase de React

---
+++ React/projects/hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
+export function App () {
+ /*  const format = (userName) => `@${userName}` */
+
+  return (
+    <section className='App'>
+      <TwitterFollowCard userName='capricornio1397'>
+        Shawn
+      </TwitterFollowCard>
+
+      <TwitterFollowCard userName='Shawn'>
+        Tiburon Rojo 
+      </TwitterFollowCard>
+    </section>
+  )
+}
